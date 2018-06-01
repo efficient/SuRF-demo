17a239752eac2643b9157a28bdbc0fec7f66c8db
Hyeontaek Lim <hl@cs.cmu.edu>
2018-06-01 12:34:30 -0400

    Add -s ALLOW_MEMORY_GROWTH=1

--- build_js.sh
+++ build_js.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-em++ -o docs/surf.js js.cpp -std=c++11 -msse2 -DNDEBUG --llvm-opts 2 --bind -v
+em++ -o docs/surf.js js.cpp -std=c++11 -msse2 -DNDEBUG --llvm-opts 2 --bind -s ALLOW_MEMORY_GROWTH=1 -v
